a26b5eab943d67b48f6c2634a480ebe5892d3a14
c2p-cmd <sharandt19@live.com>
2022-05-29 20:01:44 +0530

    added covers to application assests

--- lib/screens/cartoon_picker.dart
+++ lib/screens/cartoon_picker.dart
@@ -16,37 +16,39 @@ final _cartoonCovers = <CartoonCover>[
   CartoonCover(
       cartoonName: batmanBeyondTitle,
       link: batmanBeyond,
-      imageLink:
-          "https://i0.wp.com/batman-news.com/wp-content/uploads/2022/04/Batman-Beyond-Neo-Year-1-1-1.jpg?fit=1987%2C3056&quality=80&strip=info&ssl=1"),
+      imageLink: "assests/covers/batmanBeyond.png"
+      // "https://i0.wp.com/batman-news.com/wp-content/uploads/2022/04/Batman-Beyond-Neo-Year-1-1-1.jpg?fit=1987%2C3056&quality=80&strip=info&ssl=1"
+      ),
   CartoonCover(
       cartoonName: spongebobTitle,
       link: spongebob,
-      imageLink:
-          "https://images.viacbs.tech/uri/mgid:arc:imageassetref:nick.com:9cd2df6e-63c7-43da-8bde-8d77af9169c7?quality=0.7"),
-  // CartoonCover(
-  //     cartoonName: johnnyBravoTitle,
-  //     link: johnnyBravo,
-  //     imageLink: "https://flxt.tmsimg.com/assets/p413071_b_v13_ac.jpg"),
+      imageLink: "assests/covers/spongebob.png"
+      // "https://images.viacbs.tech/uri/mgid:arc:imageassetref:nick.com:9cd2df6e-63c7-43da-8bde-8d77af9169c7?quality=0.7"
+      ),
   CartoonCover(
       cartoonName: courageDogTitle,
       link: courageDog,
-      imageLink:
-          "https://m.media-amazon.com/images/M/MV5BMTU4MGEyNTItNzg5ZS00ZGU0LTk4NmEtODM0Y2UxYTY2YTUyXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_FMjpg_UX1000_.jpg"),
+      imageLink: "assests/covers/courage.png"
+      // "https://m.media-amazon.com/images/M/MV5BMTU4MGEyNTItNzg5ZS00ZGU0LTk4NmEtODM0Y2UxYTY2YTUyXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_FMjpg_UX1000_.jpg"
+      ),
   CartoonCover(
       cartoonName: ben10Title,
       link: ben10,
-      imageLink:
-          "https://m.media-amazon.com/images/M/MV5BNDI5NjAxMWEtNGYyOS00MjIxLTg1Y2MtMjdiZTM3NWZlYjM5XkEyXkFqcGdeQXVyMTM0NTUzNDIy._V1_.jpg"),
+      imageLink: "assests/covers/ben10.png"
+      //"https://m.media-amazon.com/images/M/MV5BNDI5NjAxMWEtNGYyOS00MjIxLTg1Y2MtMjdiZTM3NWZlYjM5XkEyXkFqcGdeQXVyMTM0NTUzNDIy._V1_.jpg"
+      ),
   CartoonCover(
       cartoonName: ben10AlienForceTitle,
       link: ben10AlienForce,
-      imageLink:
-          "https://m.media-amazon.com/images/M/MV5BYmQwYTc1ZDEtMzU3My00OTIzLWE1YmEtYmUyMmMzZTI2ZWNlXkEyXkFqcGdeQXVyOTgwMzk1MTA@._V1_.jpg"),
+      imageLink: "assests/covers/ben10AlienForce.png"
+      //"https://m.media-amazon.com/images/M/MV5BYmQwYTc1ZDEtMzU3My00OTIzLWE1YmEtYmUyMmMzZTI2ZWNlXkEyXkFqcGdeQXVyOTgwMzk1MTA@._V1_.jpg"
+      ),
   CartoonCover(
       cartoonName: ben10UltimateAlienTitle,
       link: ben10UltimateAlien,
-      imageLink:
-          "https://m.media-amazon.com/images/M/MV5BNzgxYjcwNDUtZTcxZS00NmIyLWI3OTAtNTQ3NzZjM2RkMTFkXkEyXkFqcGdeQXVyNjk1Njg5NTA@._V1_.jpg"),
+      imageLink: "assests/covers/ben10UltimateAlien.png"
+      //"https://m.media-amazon.com/images/M/MV5BNzgxYjcwNDUtZTcxZS00NmIyLWI3OTAtNTQ3NzZjM2RkMTFkXkEyXkFqcGdeQXVyNjk1Njg5NTA@._V1_.jpg"
+      ),
 ];
 
 class CartoonPicker extends StatefulWidget {
@@ -62,11 +64,6 @@ class _CartoonPickerState extends State<CartoonPicker> {
 
   @override
   void initState() {
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      for (var cartoon in _cartoonCovers) {
-        precacheImage(NetworkImage(cartoon.imageLink), context);
-      }
-    });
     super.initState();
     SystemChrome.setPreferredOrientations(
         <DeviceOrientation>[DeviceOrientation.portraitUp]);
@@ -106,18 +103,24 @@ class _CartoonPickerState extends State<CartoonPicker> {
                   height: height * 0.799,
                   viewportFraction: 0.9,
                   enlargeCenterPage: true,
-                  autoPlay: false,
+                  autoPlay: true,
                   onPageChanged: (value, _) => setState(() => _current = value),
                 ),
                 items: _cartoonCovers
                     .map((item) => Column(
                           children: [
-                            Image.network(
+                            Image.asset(
                               item.imageLink,
                               fit: BoxFit.contain,
                               height: height * 0.6,
                               width: width * 0.9,
                             ),
+                            // Image.network(
+                            //   item.imageLink,
+                            //   fit: BoxFit.contain,
+                            //   height: height * 0.6,
+                            //   width: width * 0.9,
+                            // ),
                             const Padding(padding: EdgeInsets.only(top: 18.0)),
                             TextButton(
                               clipBehavior: Clip.hardEdge,
